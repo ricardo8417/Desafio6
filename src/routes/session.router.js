506f58eda49b607794cc2b7ac90ea517e1e72786
Ricardo <rnavarrete_cervantes@hotmail.com>
2023-08-13 21:46:47 -0600

    Actualización para asignar roles de usuario y admin en automatico

--- src/routes/session.router.js
+++ src/routes/session.router.js
@@ -14,17 +14,29 @@ router.post("/login", async (req, res) => {
 });
 
 router.post("/register", async (req, res) => {
+  try{
   const user = req.body;
+const email = user.email
+const  userRol= await UserModel.findOne({email}).lean().exec();
+
+if(!userRol){
 
   if (
     user.email === "adminCoder@coder.com" &&
     user.password === "adminCod3r123"
   ) {
     user.rol = "admin";
+  }else{
+    user.rol="usuario"
   }
   await UserModel.create(user);
-
   return res.redirect("/");
+}else{
+  res.send('El usuario ya existe, favor de agregar otro')
+}
+}catch(e){
+  console.log(e)
+}
 });
 
 router.get("/logout", (req, res) => {
